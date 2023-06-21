317984e5838d28603354a313c97af26ce0f06d7c
msa <msai.sadehi@gmail.com>
2023-06-21 15:01:13 +0330

    first session

---
+++ session1.py
@@ -0,0 +1,24 @@
+# print("Hello world")
+# print("2 + 3")
+# print(2 + 3)
+# score = 10
+# print("your score is:", score)
+
+# score1 = 12
+# score_1 = 12
+# score_ = 12
+# _score = 12
+
+# number_one = 12
+
+# numberOne = 12
+
+# number = 1
+# print(type(number))
+# number = 1.3
+# print(type(number))
+
+result = 5 + 2 * 3
+print(result)
+result = (5 + 2) * 3
+print(result)
